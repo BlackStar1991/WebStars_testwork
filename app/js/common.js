87bf8a2e96be880251ff64d35a4e7afb892bfcde
BlackStar1991 <zirkaandry@ukr.net>
2017-06-17 23:10:04 +0300

    product item

--- app/js/common.js
+++ app/js/common.js
@@ -4,12 +4,20 @@ $(document).ready(function () {
    var body = $("body");
    var infoButton = $(".nav_icon");
    var fullNavigation = $(".nav_fullNavigation");
+   var navigationButton = $(".bl_navigation__button");
+   var navagationBlock = $(".bl_navigation__fullItems");
 
-   infoButton.on("click", function(){
-       fullNavigation.slideToggle(400);
+   function slideShow(button, element){
+       button.on("click", function(){
+           element.slideToggle(400);
+       });
+   }
+    slideShow(infoButton, fullNavigation);
+    slideShow(navigationButton, navagationBlock);
 
 
-   })
+
+
 
 //
 // $(window).resize(function(){

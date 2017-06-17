71e656d09258dd970f36312d9731eb555c3e9ca0
BlackStar1991 <zirkaandry@ukr.net>
2017-06-17 16:03:44 +0300

    header

--- app/js/common.js
+++ app/js/common.js
@@ -1,30 +1,25 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-	//Цели для Яндекс.Метрики и Google Analytics
-	$(".count_element").on("click", (function() {
-		ga("send", "event", "goal", "goal");
-		yaCounterXXXXXXXX.reachGoal("goal");
-		return true;
-	}));
 
-	//jquery.matchHeight.js
-	//Docs: https://github.com/liabru/jquery-match-height
-	$('element').matchHeight();
+   var body = $("body");
+   var infoButton = $(".nav_icon");
+   var fullNavigation = $(".nav_fullNavigation");
 
-	//AJAX forms send
-	//Docs: http://api.jquery.com/jquery.ajax/
-	$("form").submit(function() {
-		$.ajax({
-			type: "GET",
-			url: "mail.php",
-			data: $("form").serialize()
-		}).done(function() {
-			alert("Спасибо за заявку!");
-			setTimeout(function() {
-				$.fancybox.close();
-			}, 1000);
-		});
-		return false;
-	});
+   infoButton.on("click", function(){
+       fullNavigation.slideToggle(400);
+
+
+   })
+
+//
+// $(window).resize(function(){
+//    if( body.width() >= 992 ){
+//         fullNavigation.css("display", "block");
+//    }
+// });
+//
+//
+// $(window).resize();
+
 
 });
